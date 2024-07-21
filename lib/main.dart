9abacee1d687a61ba26b3159391a1141fe5df61d
PMYLS <mfahid1993@gmail.com>
2024-07-22 01:03:00 +0500

    Listview with Builder

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _names = ['abd','bbb','abd','bbb','abd','bbb','abd','bbb','abd','bbb',];
+
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +39,67 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.amber,
         title: const Text('ListView...'),
       ),
+        //reverse: true,
+        /*children:  const [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('One', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('two', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('three', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('four', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('five', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('six', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('seven', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('eight', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('nine', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('ten', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+          ),
+
+
+        ],*/
+        // ListView builder method
+      body: ListView.builder(itemBuilder: itemBuilder,
+      itemCount: _names.length,
+        scrollDirection: Axis.horizontal,
+        itemExtent: 100.0,
+      ),
+
     );
   }
+
+  Widget? itemBuilder(BuildContext context, int index) {
+
+    return  Padding(padding: EdgeInsets.all(16.9),
+        child: Text(_names[index], style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400))
+    );
+
+  }
+
 }
